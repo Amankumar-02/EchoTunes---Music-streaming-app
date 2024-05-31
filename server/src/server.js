1ac0c4a901684a23dd9d0c523edf7cc55a04f752
Aman <ak193566@gmail.com>
2024-05-31 21:00:05 +0530

    update

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import {app, port} from './app.js';
 import DB_Connect from './db/index.js';
 import {readDirectory} from './controllers/media.controller.js';
 
-dotenv.config({path: "./.env"});
+dotenv.config({path: ".env"});
 
 DB_Connect().then(()=>{
     readDirectory();
